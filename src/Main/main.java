4181c3f1ff1059eb1a85b70526d964c4dc29f9c9
vava <Haisv@192.168.1.173>
2025-09-16 11:40:09 -0600

    V 1.0 - Main terminado, funciones con oH1 y oH2.

--- src/Main/main.java
+++ src/Main/main.java
@@ -5,6 +5,9 @@
 package Main;
 
 import Clases.Hospital;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
@@ -16,7 +19,52 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner MyScanner = new Scanner(System.in);
+        //Objeto oH1:
+            Hospital oH1 = new Hospital();
+            System.out.println("idHospital: ");
+            int id_hospital = MyScanner.nextInt();
+            MyScanner.nextLine();
+            System.out.println("Nombre: ");
+            String nombre = MyScanner.nextLine();
+            System.out.println("Direccion: ");
+            String direccion = MyScanner.nextLine();
+            System.out.println("Num Doctores: ");
+            int num_doctores = MyScanner.nextInt();
+            System.out.println("Num Pacientes: ");
+            int num_pacientes = MyScanner.nextInt();
+            System.out.println("Costo Consulta: ");
+            double costo_consulta = MyScanner.nextDouble();
+            oH1.setIdHospital(id_hospital);
+            oH1.setNombre(nombre);
+            oH1.setDireccion(direccion);
+            oH1.setNumDoctores(num_doctores);
+            oH1.setNumPacientes(num_pacientes);
+            oH1.setCostoConsulta(costo_consulta);
+        //Objeto oH2:
+            Hospital oH2 = new Hospital(1,"Cruz Roja","Calle 3",5,7,530.50);
+        //Mostrar los datos de oH1 utilizando getters:
+            System.out.println("HOSPITAL 1.");
+            System.out.println("idHospital: "+ oH1.getIdHospital());
+            System.out.println("Nombre: "+ oH1.getNombre());
+            System.out.println("Direccion: "+ oH1.getDireccion());
+            System.out.println("Num Doctores: "+ oH1.getNumDoctores());
+            System.out.println("Num Pacientes: "+ oH1.getNumPacientes());
+            System.out.println("Costo Consulta: "+ oH1.getCostoConsulta());
+            System.out.println("=====================================");
+        //Mostrar los datos de oH2 utilizando toString():
+            System.out.println("HOSPITAL 2.");
+            System.out.println(oH2.toString());
+            System.out.println("=====================================");
+        //Mostrar resultados de las operaciones.
+            System.out.println("HOSPITAL 1.");
+            System.out.println("Ingresos: "+ oH1.calcularIngresos());
+            System.out.println("Pacientes*Doctor: "+ oH1.promedioPacientes_Doctor());
+            System.out.println("=====================================");
+            System.out.println("HOSPITAL 2.");
+            System.out.println("Ingresos: "+ oH2.calcularIngresos());
+            System.out.println("Pacientes*Doctor: "+ oH2.promedioPacientes_Doctor());
+            System.out.println("=====================================");
     }
         
 }
